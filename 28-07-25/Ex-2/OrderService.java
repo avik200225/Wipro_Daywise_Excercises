c02b54ed2a38077b8d71cd84ae4992b6eda8daef
avik200225 <157623260+avik200225@users.noreply.github.com>
2025-08-10 00:21:20 +0530

    Add files via upload

---
+++ 28-07-25/Ex-2/OrderService.java
@@ -0,0 +1,7 @@
+package com.WiproA.mockitodemo;
+
+public class OrderService {
+	    public String placeOrder(String orderItem) {
+	        return null;
+	    }
+}
---
+++ 28-07-25/Ex-2/OrderServiceTest.java
@@ -0,0 +1,19 @@
+package com.WiproA.mockitodemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class OrderServiceTest {
+	 @Test
+	    void test() {
+
+	        OrderService mockOrderService = Mockito.mock(OrderService.class);
+	        when(mockOrderService.placeOrder("ORD-01")).thenReturn("successful");
+	        String result = mockOrderService.placeOrder("ORD-01");
+	        assertEquals("successful", result);
+	        verify(mockOrderService).placeOrder("ORD-01");
+}
+}
