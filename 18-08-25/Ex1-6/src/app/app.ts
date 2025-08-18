a9ff691af4c8e1f43fce41a6eccda8395fb02010
avik200225 <157623260+avik200225@users.noreply.github.com>
2025-08-18 21:25:54 +0530

    Add files via upload

---
+++ 18-08-25/Ex1-6/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Secondcomp } from './secondcomp/secondcomp';
+
+@Component({
+  selector: 'app-root',
+  imports: [Secondcomp],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('firstapp');
+}
---
+++ 18-08-25/Ex1-6/src/app/thirdcomp/thirdcomp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thirdcomp',
+  imports: [],
+  template: `<p> thirdcomp works!</p> <h3> WELCOME TO THIRD COMPONENT</h3>`,
+  styleUrl: './thirdcomp.css'
+})
+export class Thirdcomp {
+
+}
